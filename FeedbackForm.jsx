e0af366b93859a80b0b6a20c10e0efcee9cb7ed8
mithun2k2 <mithun2k2eee@gmail.com>
2024-08-17 19:03:12 +0100

    New functionality added to feedback form

--- FeedbackForm.jsx
+++ FeedbackForm.jsx
@@ -6,6 +6,7 @@ const[formData, setFormData]= useState({
   name: '',
   email: '',
   feedback: '',
+  rating: ''
 })
 const handleChange = (event) =>{
   const {name, value} = event.target;
@@ -20,6 +21,7 @@ const handleSubmit = (event) =>{
   Name: ${formData.name}
   Email: ${formData.email}
   Feedback: ${formData.feedback}
+  Rating: ${formData.rating}
   `;
   const isConfirmed = window.confirm(`Please confirm your details: \n\n ${confirmationMessage}`)
 if(isConfirmed){
@@ -27,7 +29,8 @@ if(isConfirmed){
   setFormData({
    name: '',
    email: '',
-   feedback: ''
+   feedback: '',
+   rating:''
   });
   alert('Thank you for your valuable feedback!');
   }
@@ -58,6 +61,44 @@ if(isConfirmed){
         value = {formData.feedback}
         onChange = {handleChange}
         ></textarea>
+        <div style = {{display: 'flex', gap: '10px', flexDirection: 'column'}}>
+        <span>Rate Us</span>
+        <p><input
+            type="radio"
+            name="rating"
+            value="1"
+            onChange={handleChange}
+        
+        /> 1</p>
+        <p><input
+            type="radio"
+            name="rating"
+            value="2"
+            onChange={handleChange}
+        
+        /> 2</p>
+        <p><input
+            type="radio"
+            name="rating"
+            value="3"
+            onChange={handleChange}
+        
+        /> 3</p>
+        <p><input
+            type="radio"
+            name="rating"
+            value="4"
+            onChange={handleChange}
+        
+        /> 4</p>
+        <p><input
+            type="radio"
+            name="rating"
+            value="5"
+            onChange={handleChange}
+        
+        /> 5</p>
+        </div>
         <button type ="submit">Submit Feedback</button>
       </form>
     </>
